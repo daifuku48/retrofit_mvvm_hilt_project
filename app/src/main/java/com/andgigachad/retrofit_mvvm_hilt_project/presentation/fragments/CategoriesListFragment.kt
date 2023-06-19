2aafb9be19c262c831f079ef23e193713c75bd8c
danil <natalya.haritonov.72@gmail.com>
2023-06-20 00:03:46 +0300

    bugs

--- app/src/main/java/com/andgigachad/retrofit_mvvm_hilt_project/presentation/fragments/CategoriesListFragment.kt
+++ app/src/main/java/com/andgigachad/retrofit_mvvm_hilt_project/presentation/fragments/CategoriesListFragment.kt
@@ -6,19 +6,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.andgigachad.retrofit_mvvm_hilt_project.data.repository.FavoriteMealRepositoryImpl
 import com.andgigachad.retrofit_mvvm_hilt_project.databinding.FragmentCategoriesListBinding
+import com.andgigachad.retrofit_mvvm_hilt_project.domain.repository.FavoriteMealRepository
 import com.andgigachad.retrofit_mvvm_hilt_project.presentation.viewmodels.MainViewModel
+import com.andgigachad.retrofit_mvvm_hilt_project.presentation.viewmodels.MainViewModelFactory
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class CategoriesListFragment : Fragment() {
 
-    private val vm: MainViewModel by viewModels()
+
+
     private var _binding : FragmentCategoriesListBinding? = null
 
     private val binding
         get() = _binding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
--- app/src/main/java/com/andgigachad/retrofit_mvvm_hilt_project/presentation/viewmodels/MainViewModelFactory.kt
+++ app/src/main/java/com/andgigachad/retrofit_mvvm_hilt_project/presentation/viewmodels/MainViewModelFactory.kt
@@ -1,15 +1,31 @@
 package com.andgigachad.retrofit_mvvm_hilt_project.presentation.viewmodels
 
+import android.os.Bundle
+import androidx.lifecycle.AbstractSavedStateViewModelFactory
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.savedstate.SavedStateRegistryOwner
+import com.andgigachad.retrofit_mvvm_hilt_project.data.repository.FavoriteMealRepositoryImpl
 import com.andgigachad.retrofit_mvvm_hilt_project.domain.repository.FavoriteMealRepository
+import com.andgigachad.retrofit_mvvm_hilt_project.network.RetrofitService
+import com.andgigachad.retrofit_mvvm_hilt_project.network.mappers.CategoriesMapper
 
 class MainViewModelFactory(
-    private val savedStateHandle: SavedStateHandle,
-    private val repository: FavoriteMealRepository
-) : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return MainViewModel(savedStateHandle, repository) as T
+    owner: SavedStateRegistryOwner,
+    defaultArgs: Bundle? = null
+) : AbstractSavedStateViewModelFactory(owner, defaultArgs) {
+
+    private val repository by lazy(LazyThreadSafetyMode.NONE){
+        FavoriteMealRepositoryImpl(recipeService = RetrofitService,
+        mapper = CategoriesMapper())
+    }
+
+    override fun <T : ViewModel> create(
+        key: String, modelClass: Class<T>, handle: SavedStateHandle
+    ): T {
+        return MainViewModel(
+            repository, handle
+        ) as T
     }
 }
