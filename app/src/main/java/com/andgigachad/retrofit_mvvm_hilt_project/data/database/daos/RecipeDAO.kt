37be1a6b8da63bc7950ca0adb3ebb0e635a24acc
danil <natalya.haritonov.72@gmail.com>
2023-07-12 19:14:49 +0300

    Adding Room

---
+++ app/src/main/java/com/andgigachad/retrofit_mvvm_hilt_project/data/database/daos/RecipeDAO.kt
@@ -0,0 +1,19 @@
+package com.andgigachad.retrofit_mvvm_hilt_project.data.database.daos
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.andgigachad.retrofit_mvvm_hilt_project.data.database.entities.RecipeEntity
+
+@Dao
+interface RecipeDAO{
+    @Insert
+    suspend fun insert(recipeEntity: RecipeEntity)
+
+    @Delete
+    suspend fun delete(recipeEntity: RecipeEntity)
+
+    @Query("SELECT * FROM RECIPE_TABLE")
+    suspend fun getAllRecipe() : List<RecipeEntity>
+}
