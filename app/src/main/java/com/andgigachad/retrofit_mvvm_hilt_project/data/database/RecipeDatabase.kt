e11d8af8dbcc207c8429f62190f378d83c592f82
danil <natalya.haritonov.72@gmail.com>
2023-07-30 19:50:25 +0300

    end

--- app/src/main/java/com/andgigachad/retrofit_mvvm_hilt_project/data/database/RecipeDatabase.kt
+++ app/src/main/java/com/andgigachad/retrofit_mvvm_hilt_project/data/database/RecipeDatabase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 import com.andgigachad.retrofit_mvvm_hilt_project.data.database.daos.RecipeDAO
 import com.andgigachad.retrofit_mvvm_hilt_project.data.database.entities.RecipeEntity
 
-@Database(entities = [RecipeEntity::class], version = 7)
+@Database(entities = [RecipeEntity::class], version = 8, exportSchema = false)
 abstract class RecipeDatabase : RoomDatabase() {
     abstract fun recipeDAO() : RecipeDAO
 }
